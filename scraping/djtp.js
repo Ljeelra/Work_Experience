93b0ddf733f3dbad35936c267156225c011bede1
ljeelra <dlwldms9803@gmail.com>
2024-11-21 11:56:04 +0900

    refactor: 스크랩 사이트의 url과 구조 변경으로 코드를 전체적으로 수정

--- scraping/djtp.js
+++ scraping/djtp.js
@@ -10,8 +10,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const chunkSize = 50;
-const baseUrl = 'https://www.djtp.or.kr/sub010101';
-const detailBaseUrl = 'https://www.djtp.or.kr/sub010101/view/id/';
+const baseUrl = 'https://www.djtp.or.kr/pbanc?mid=a20101000000&nPage=';
+const baseUrl2 = 'https://www.djtp.or.kr/board.es?mid=a20102000000&bid=0102&nPage=';
+const detailBaseUrl = 'https://www.djtp.or.kr/board.es?mid=a20102000000&bid=0102&act=view&list_no=';
 const row = 10;
 const axiosInstance = axios.create({
     timeout: 60000, // 60초 타임아웃
@@ -38,100 +39,157 @@ const axiosInstance = axios.create({
 });
 
 async function getPathIds(){
-    const pathIds = [];
+    const pathData = [];
     let page = 1;
-    while (true) {
+    while(true){
         try{
-            //console.log(`${page}페이지 pathid 추출 시작합니다`);
-            const listUrl = `${baseUrl}/index/page/${page}`;
+            const listUrl = `${baseUrl}${page}`;
             const response = await axiosInstance.get(listUrl);
             const $ = cheerio.load(response.data);
 
             let stopExtraction = false;
+
+            $('#board-list tbody tr').each((index, element)=> {
+                const thirdTd = $(element).find('td').eq(3);
+                const status = thirdTd.find('span.status.end[data-label="마감"]');
+                
+                if (status.length > 0) {
+                    stopExtraction = true;
+                    return false;
+                }
+
+                const secondTd = $(element).find('td').eq(2);
+                const pathId = secondTd.find('strong').text().trim();
+                if(pathId){
+                    const rowData = {
+                        pathId: pathId,
+                        category: null,
+                        title: null,
+                        requestStartedOn: null,
+                        requestEndedOn: null,
+                        department: null,
+                        attachmentFile: null
+                    };
+                    rowData.category = $(element).find('td[aria-label="유형"]').text().trim();
+                    rowData.title = $(element).find('td[aria-label="공고명"] a').text().trim();
+                    const dateTerm = $(element).find('td[aria-label="접수기간"]').text().trim();
+                    const date = dateTerm.split('~');
+                    rowData.requestStartedOn = date[0].replace(/\./g, '-');
+                    rowData.requestEndedOn = date[1].replace(/\./g, '-');
+                    rowData.department = $(element).find('td[aria-label="부서"]').text().trim();
+                    rowData.attachmentFile = $(element).find('td[aria-label="공고명"] a').attr('href');
+                    pathData.push(rowData);
+                }
+                
+            });
+
+            if (stopExtraction) {
+                //console.log('pathId 추출이 종료되었습니다.');
+                break;
+            }
+
+            page++;
+
+        }catch(error){
+            console.error(`djtp.js getPathIds error: `,error)
+
+        }
+    }
+    return pathData;
+}
+
+async function getgPathIds(){
+    const pathData = [];
+    let page = 1;
+    while (true) {
+        try{
+            //console.log(`${page}페이지 pathid 추출 시작합니다`);
+            const listUrl2 = `${baseUrl2}${page}`;
+            const response = await axiosInstance.get(listUrl2);
+            const $ = cheerio.load(response.data);
+
+            let stopExtraction = false;
     
-            $('tbody tr').each((index, element) => {
-                const status = $(element).find('td.subject span.progress.progress2');
+            $('#board-list tbody tr').each((index, element) => {
+                const status = $(element).find('td span.status.small.end:contains("완료")');
                 if (status.length > 0) {
                     stopExtraction = true;
                     return false; // each 루프 중단
                 }
 
-                const href = $(element).find('td.subject a').attr('href'); // href 속성 값을 가져옵니다.
-                const hrefMatch = href.match(/id\/(\d+)/); // 정규 표현식으로 seq 값을 추출합니다.
+                const onclick = $(element).find('td[aria-label="제목"] a').attr('onclick');
+                const regex = /goView\('(\d+)'\)/;
+                const onclickMatch = onclick.match(regex);
 
-                if (hrefMatch) {
-                    const pathId = hrefMatch[1]; // 추출된 seq 값을 가져옵니다.
-                    pathIds.push(pathId);
-                    //console.log(seqValue); // seq 값을 출력합니다.
+                if (onclickMatch) {
+                    const gpathId = onclickMatch[1];
+                    const rowData = {
+                        pathId: gpathId,
+                        category: null,
+                        title: null,
+                        requestStartedOn: null,
+                        requestEndedOn: null,
+                        announcementDate: null,
+                        attachmentFile: null
+                    };
+                    rowData.category = $(element).find('td').eq(2).text().trim();
+                    rowData.title = $(element).find('td[aria-label="제목"] a').text().trim();
+                    const dateTerm = $(element).find('td').eq(4).text().trim();
+                    let [startDate, endDate] = dateTerm.split('~');
+                    endDate = endDate.replace(/\s?\(D-\d+\)/, '').trim();
+                    rowData.requestStartedOn = startDate.replace(/\//g, '-');
+                    rowData.requestEndedOn = endDate.replace(/\//g, '-');
+                    let aDate = $(element).find('td[aria-label="등록일"]').text().trim();
+                    rowData.announcementDate = aDate.replace(/\//g, '-');
+                    pathData.push(rowData);
                 }
 
             });       
             
             if (stopExtraction) {
-                console.log('pathId 추출이 종료되었습니다.');
-                break; // while 루프 중단
+                //console.log('pathId 추출이 종료되었습니다.');
+                break;
             }
     
-            //console.log(pathIds);
             page++;
+    
         } catch(error){
-            console.error('djtp.getListPathIds() 에러 발생: ',error);
+            console.error('djtp.getPathIds() 에러 발생: ',error);
         }
 
     }
-    return pathIds;
+
+    return pathData;
 }
 
 async function filterPathId(scrapedData, siteName) {
     try {
         const existingPathIds = await getAllPathIds(siteName);
-        // console.log('Scraped Data:', scrapedData);
-        // console.log('Existing Path IDs:', existingPathIds);
-        
+        //console.log('Existing Path IDs:', existingPathIds);  확인을 위한 로그
         if (!Array.isArray(existingPathIds)) {
             throw new Error('Existing Path IDs is not an array');
         }
-        return scrapedData.filter(pathId => !existingPathIds.includes(pathId));
+        return scrapedData.filter(data => !existingPathIds.includes(data.pathId));
     } catch (error) {
-        console.error('Error fetching existing path IDs:', error);
+        console.error('jungsoventure Error fetching existing path IDs:', error);
         return []; // 오류 발생 시 빈 배열 반환
     }
 }
 
-async function scrapeDetailPage(pathId, siteName){
-    const data={
-        title:null,
-        site: siteName,
-        pathId: pathId,
-        announcementDate: null,
-        requestStartedOn: null,
-        requestEndedOn: null,
-        attachmentFile: [],
-        contents: null,
-        contentImage: []
-    };
+
+async function scrapeDetailFile(pathId, siteName){
+    const attachmentFile =[];
     try{
         const detailUrl = `${detailBaseUrl}${pathId}`
         const detailHtml = await axiosInstance.get(detailUrl);
         const $ = cheerio.load(detailHtml.data);
 
-        data.title = $('div.newsfeed-view').find('strong.newsfeed-subject').text().trim();
-        // console.log(data.title);
-        data.announcementDate = $('div.newsfeed-info').find('dd').eq(1).text().trim();
-        const dateTerm = $('div.newsfeed-info').find('dd').eq(4).text().trim();
-        const resultDate = dateTerm.replace(/\(.*?\)/g, '').trim();
-        const applyDate = resultDate.split('~');
-        data.requestStartedOn = applyDate[0]?.trim() || 'N/A';
-        data.requestEndedOn = applyDate[1]?.trim() || 'N/A';
-   
-
-
-        const file = $('ul.file-list');
+        const file = $('div.file-wrap');
         file.find('a').each((index, element) => {
             const fileNm = $(element).text().trim();
             const fileHref = $(element).attr('href');
             const fileLink = fileHref.startsWith('https://') ? fileHref : fileHref.startsWith('/') ? `https://www.djtp.or.kr${fileHref}` : null;
-            data.attachmentFile.push({
+            attachmentFile.push({
                 fileNm: fileNm,
                 fileLink: fileLink
             });
@@ -139,8 +197,8 @@ async function scrapeDetailPage(pathId, siteName){
         });
         
 
-        //console.log(data);
-        return data;
+        //console.log(attachmentFile);
+        return attachmentFile;
     }catch(error){
         console.error(`djtp.scrapeDetailPage() 에러: ${error.message}`, error);
     }
@@ -153,28 +211,46 @@ async function delay(ms) {
 async function djtp(){
     const siteName = 'djtp';
     try{
-        const pathIds = await getPathIds();
-        //console.log(pathIds);
+        const rowData = await getPathIds(); 
+        const growData = await getgPathIds();
 
-        const filterPathIds = await filterPathId(pathIds,siteName);
-        if (filterPathIds.length === 0) {
+        const filterData = await filterPathId(rowData,siteName);
+        if (filterData.length === 0) {
             console.log('모든 데이터가 필터링되었습니다. 새로운 데이터가 없습니다.');
             return;
         }
-        console.log(`필터링된 후 데이터 개수: ${filterPathIds.length}`);
+        console.log(`필터링된 후 사업공고 데이터 개수: ${filterData.length}`);
 
-        const filteredDataResults = [];
-        console.log(`상세페이지 스크랩 시작합니다`);
-        for (const pathId of filterPathIds) {
-            const data = await scrapeDetailPage(pathId, siteName);
+        const filterGdata = await filterPathId(growData,siteName);
+        if (filterGdata.length === 0) {
+            console.log('모든 데이터가 필터링되었습니다. 새로운 데이터가 없습니다.');
+            return;
+        }
+        console.log(`필터링된 후 일반사업공고 데이터 개수: ${filterGdata.length}`);
+        console.log(`추가될 데이터 개수 : `,filterData.length+filterGdata.length);
+        //console.log(filterGdata);
+        const gDataResults = [];
+        console.log(`일반공고 상세페이지 스크랩 시작합니다`);
+        
+        for (const gData of filterGdata) {
+            const { pathId } = gData;
+         
+            //상세 페이지에서 첨부파일 정보를 가져오기
+            const data = await scrapeDetailFile(pathId, siteName);
+            
             if (data !== null) {
-                filteredDataResults.push(data);
-                await delay(2000); // 2초 딜레이 추가
+                // 데이터에서 첨부파일 정보를 gData에 추가
+                gData.attachmentFile = data;  // 대소문자 일관성 맞추기
+                gDataResults.push(gData);  // 업데이트된 gData를 결과 배열에 추가
+                await delay(2000);  // 2초 딜레이 추가
             }
         }
 
+        //console.log(gDataResults);
+
         // 데이터 저장
-        await saveDataInChunks(filteredDataResults, siteName);
+        await saveDataInChunks(filterData, siteName);
+        await saveDataInChunks(gDataResults, siteName);
     }catch(error){
         console.error(`djtp() 에러, ${error.message}:`, error);
     }
@@ -198,5 +274,5 @@ async function saveDataInChunks(data, siteName) {
     }
 }
 
-
+//djtp();
 export default djtp;
